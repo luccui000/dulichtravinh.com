e73edcaca48031d018ddf59551027106a84b8111
luccui000 <luccui2k@gmail.com>
2022-01-24 14:29:55 +0700

    ADD: create index layout

---
+++ index.php
@@ -0,0 +1,309 @@
+<?php
+
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Du lịch Trà Vinh</title>
+    <link rel="stylesheet" href="./assets/reset.css">
+    <link rel="stylesheet" href="./assets/style.css">
+</head>
+<body>
+    <header class="travinh__header">
+        <nav class="travinh__navbar grid">
+            <div class="travinh__logo">
+                <h1>.TRA VINH</h1>
+            </div>
+            <div class="travinh__menu">
+                <ul class="travinh__menu--list">
+                    <li class="travinh__menu--item">
+                        <a href="" class="travinh__menu--link">Trang chủ</a>
+                    </li>
+                    <li class="travinh__menu--item">
+                        <a href="" class="travinh__menu--link">Du lịch</a>
+                    </li>
+                    <li class="travinh__menu--item">
+                        <a href="" class="travinh__menu--link">Ẩm thực</a>
+                        <ul class="travinh__menu--sublist">
+                            <li class="travinh__menu--subitem">
+                                <a href="" class="travinh__menu--sublink">Món ăn truyền thống</a>
+                            </li>
+                            <li class="travinh__menu--subitem">
+                                <a href="" class="travinh__menu--sublink">Món ăn đường phố</a>
+                            </li>
+                            <li class="travinh__menu--subitem">
+                                <a href="" class="travinh__menu--sublink">Món ăn vặt</a>
+                            </li>
+                        </ul>
+                    </li>
+                    <li class="travinh__menu--item">
+                        <a href="" class="travinh__menu--link">Dịch vụ</a>
+                    </li>
+                    <li class="travinh__menu--item">
+                        <div class="travinh_switchlang">
+                            <img id="switch-language" src="./assets/images/vietnam.png" alt="Vietnam">
+                        </div>
+                    </li>
+                </ul>
+            </div>
+        </nav>
+    </header>
+    <main class="travinh__main grid">
+        <div class="travinh__toppost">
+            <div
+                class="travinh__toppost--top travinh__toppost--top1"
+                style="background-image: url('https://thamhiemmekong.com/wp-content/uploads/2020/04/bien-ba-dong.jpg')"
+            >
+                <div class="travinh__toppost--content">
+                    <span class="travinh__toppost--tag">
+                        Du lịch Ba động
+                    </span>
+                    <h4 class="travinh__toppost--title">
+                        BIỂN BA ĐỘNG – BÃI BIỂN ĐẸP NHẤT XỨ TRÀ VINH
+                    </h4>
+                    <span class="travinh__toppost--info">
+                        By <strong>Lưc Cui</strong>, 19/01/2022
+                    </span>
+                </div>
+            </div>
+            <div
+                class="travinh__toppost--top travinh__toppost--top2"
+                style="background-image: url('https://images.unsplash.com/photo-1512453979798-5ea266f8880c?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80')"
+            >
+                <div class="travinh__toppost--content">
+                    <span class="travinh__toppost--tag">
+                        Du lịch Ba động
+                    </span>
+                    <h4 class="travinh__toppost--title">
+                        BIỂN BA ĐỘNG – BÃI BIỂN ĐẸP NHẤT XỨ TRÀ VINH
+                    </h4>
+                    <span class="travinh__toppost--info">
+                        By <strong>Lưc Cui</strong>, 19/01/2022
+                    </span>
+                </div>
+            </div>
+            <div
+                class="travinh__toppost--top travinh__toppost--top3"
+                style="background-image: url('https://images.unsplash.com/photo-1512453979798-5ea266f8880c?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80')"
+            >
+
+            </div>
+            <div
+                class="travinh__toppost--top travinh__toppost--top3"
+                style="background-image: url('https://images.unsplash.com/photo-1500828131278-8de6878641b8?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=687&q=80')"
+            >
+                <div class="travinh__toppost--content">
+                    <span class="travinh__toppost--tag">
+                        Du lịch Ba động
+                    </span>
+                    <h4 class="travinh__toppost--title">
+                        BIỂN BA ĐỘNG – BÃI BIỂN ĐẸP NHẤT XỨ TRÀ VINH
+                    </h4>
+                    <span class="travinh__toppost--info">
+                        By <strong>Lưc Cui</strong>, 19/01/2022
+                    </span>
+                </div>
+            </div>
+            <div
+                class="travinh__toppost--top travinh__toppost--top4"
+                style="background-image: url('https://images.unsplash.com/photo-1510665724063-f77a01074aa2?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=688&q=80')"
+            >
+                <div class="travinh__toppost--content">
+                    <span class="travinh__toppost--tag">
+                        Du lịch Ba động
+                    </span>
+                    <h4 class="travinh__toppost--title">
+                        BIỂN BA ĐỘNG – BÃI BIỂN ĐẸP NHẤT XỨ TRÀ VINH
+                    </h4>
+                    <span class="travinh__toppost--info">
+                        By <strong>Lưc Cui</strong>, 19/01/2022
+                    </span>
+                </div>
+            </div>
+        </div>
+        <div class="travinh__posts">
+            <div class="travinh__posts--content">
+                <h3 class="travinh__post--category">Du lịch</h3>
+                <div class="travinh__post--wrapper">
+                    <ul class="travinh__post--list">
+                        <li class="travinh__post--item">
+                            <a class="travinh__post--thumbnail" href="">
+                                <img src="https://images.unsplash.com/photo-1480261593581-efc3c9bf77bb?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1074&q=80" alt="">
+                            </a>
+                            <div class="travinh__post--content">
+                                <a href="#">
+                                    <div class="travinh__post--title">Biển Ba động - Bãi biển đẹp nhất Trà Vinh</div>
+                                </a>
+                                <span class="travinh__post--info">By <strong>Lực Cui</strong>, 19-01-2022</span>
+                                <div class="travinh__post--description">Biển Ba Động là danh thắng và địa điểm du lịch Trà Vinh nổi tiếng thuộc địa phận xã Trường Long Hoà, huyện Duyên Hải...</div>
+                            </div>
+                        </li>
+                        <li class="travinh__post--item">
+                            <a class="travinh__post--thumbnail" href="">
+                                <img src="https://images.unsplash.com/photo-1497968021412-a86898ccbc4a?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1074&q=80" alt="">
+                            </a>
+                            <div class="travinh__post--content">
+                                <a href="#">
+                                    <div class="travinh__post--title">Biển Ba động - Bãi biển đẹp nhất Trà Vinh</div>
+                                </a>
+                                <span class="travinh__post--info">By <strong>Lực Cui</strong>, 19-01-2022</span>
+                                <div class="travinh__post--description">Cách trung tâm TP. Trà Vinh chừng 60km, bãi biển Ba Động thơ mộng đã được người Pháp khám phá, cho xây dựng thành nơi tắm biển, nghỉ dưỡng từ những năm đầu thế kỷ XX và đặt tên là Biển Nhà Mát....</div>
+                            </div>
+                        </li>
+                        <li class="travinh__post--item">
+                            <a class="travinh__post--thumbnail" href="">
+                                <img src="https://images.unsplash.com/photo-1500531279542-fc8490c8ea4d?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1171&q=80" alt="">
+                            </a>
+                            <div class="travinh__post--content">
+                                <a href="#">
+                                    <div class="travinh__post--title">Biển Ba động - Bãi biển đẹp nhất Trà Vinh</div>
+                                </a>
+                                <span class="travinh__post--info">By <strong>Lực Cui</strong>, 19-01-2022</span>
+                                <div class="travinh__post--description">Vùng biển Đông ven bờ các tỉnh Đồng bằng sông Cửu Long là vùng biển phù sa do nhiều cửa sông lớn nhỏ đổ ra nên phần lớn là bãi bùn, nước không trong. Trong đó, biển Ba Động là khu vực hiếm hoi ở miền Tây Nam bộ được thiên nhiên hào phóng ban tặng cho một bãi cát dài hơn 10 km từ ấp Nhà Mát tới ấp Cồn Trứng....</div>
+                            </div>
+                        </li>
+                    </ul>
+                </div>
+                <h3 class="travinh__post--category">Ẩm thực</h3>
+                <div class="travinh__post--wrapper">
+                    <ul class="travinh__post--list">
+                        <li class="travinh__post--item">
+                            <a class="travinh__post--thumbnail" href="">
+                                <img src="https://images.unsplash.com/photo-1498025903933-cf27e8d97921?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1171&q=80" alt="">
+                            </a>
+                            <div class="travinh__post--content">
+                                <a href="#">
+                                    <div class="travinh__post--title">Biển Ba động - Bãi biển đẹp nhất Trà Vinh</div>
+                                </a>
+                                <span class="travinh__post--info">By <strong>Lực Cui</strong>, 19-01-2022</span>
+                                <div class="travinh__post--description">Theo những người dân sống lâu năm ở vùng đất này cho biết, sở dĩ có tên gọi bãi biển Ba Động là bởi mỗi khi thủy triều xuống, bờ biển nơi đây lại nổi lên ba động cát, gồm hai động nhỏ và một động lớn có dáng vẻ độc đáo, thu hút sự hiếu kỳ của người dân bản địa....</div>
+                            </div>
+                        </li>
+                        <li class="travinh__post--item">
+                            <a class="travinh__post--thumbnail" href="">
+                                <img src="https://images.unsplash.com/photo-1448099940878-e0c48ea3a165?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80" alt="">
+                            </a>
+                            <div class="travinh__post--content">
+                                <a href="#">
+                                    <div class="travinh__post--title">Biển Ba động - Bãi biển đẹp nhất Trà Vinh</div>
+                                </a>
+                                <span class="travinh__post--info">By <strong>Lực Cui</strong>, 19-01-2022</span>
+                                <div class="travinh__post--description">Do độ dốc thoai thoải, khi nước thủy triều xuống, bãi cát phơi ra hàng trăm mét, từ bờ xuống tới mép nước. Cũng do nằm trong khu vực biển phù sa nên bãi cát Ba Động không trắng muốt hay vàng óng ả, nước biển Ba Động cũng không thể trong xanh như với các bãi biển Miền Trung....</div>
+                            </div>
+                        </li>
+                        <li class="travinh__post--item">
+                            <a class="travinh__post--thumbnail" href="">
+                                <img src="https://images.unsplash.com/photo-1496542273972-b13a7ec3ce5a?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1171&q=80" alt="">
+                            </a>
+                            <div class="travinh__post--content">
+                                <a href="#">
+                                    <div class="travinh__post--title">Biển Ba động - Bãi biển đẹp nhất Trà Vinh</div>
+                                </a>
+                                <span class="travinh__post--info">By <strong>Lực Cui</strong>, 19-01-2022</span>
+                                <div class="travinh__post--description">Tuy nhiên, dọc bờ biển Đông, từ Gò Công tới Cà Mau, Ba Động có bãi cát đẹp, nước biển khá trong, nhất là vào những tháng sau Tết Nguyên đán, sóng yên biển lặng, hình thành khu du lịch biển được nhiều người ưa chuộng....</div>
+                            </div>
+                        </li>
+                    </ul>
+                </div>
+                <h3 class="travinh__post--category">Dịch vụ</h3>
+                <div class="travinh__post--wrapper">
+                    <div class="travinh__services">
+                        <ul class="travinh__service--cards">
+                            <li class="travinh__service--card">
+                                <a href="#" class="travinh__service--thumbnail">
+                                    <img src="https://dulichtravinh.com.vn/images/2017/09/29/tom_kho_vinh_kim.jpg" alt="">
+                                </a>
+                                <div class="travinh__service--content">
+                                    <a href="#" class="travinh__service--link">
+                                        <h4 class="travinh__service--title">Tôm khô Vĩnh Kim</h4>
+                                    </a>
+                                    <p>Giá: 140.000đ</p>
+                                    <button class="travinh__service--addtocart">Thêm vào giỏ hàng</button>
+                                </div>
+                            </li>
+                            <li class="travinh__service--card">
+                                <a href="#" class="travinh__service--thumbnail">
+                                    <img src="https://dulichtravinh.com.vn/images/2017/09/29/tom_kho_vinh_kim.jpg" alt="">
+                                </a>
+                                <div class="travinh__service--content">
+                                    <a href="#" class="travinh__service--link">
+                                        <h4 class="travinh__service--title">Tôm khô Vĩnh Kim</h4>
+                                    </a>
+                                    <p>Giá: 140.000đ</p>
+                                    <button class="travinh__service--addtocart">Thêm vào giỏ hàng</button>
+                                </div>
+                            </li>
+                            <li class="travinh__service--card">
+                                <a href="#" class="travinh__service--thumbnail">
+                                    <img src="https://dulichtravinh.com.vn/images/2017/09/29/tom_kho_vinh_kim.jpg" alt="">
+                                </a>
+                                <div class="travinh__service--content">
+                                    <a href="#" class="travinh__service--link">
+                                        <h4 class="travinh__service--title">Tôm khô Vĩnh Kim</h4>
+                                    </a>
+                                    <p>Giá: 140.000đ</p>
+                                    <button class="travinh__service--addtocart">Thêm vào giỏ hàng</button>
+                                </div>
+                            </li>
+                            <li class="travinh__service--card">
+                                <a href="#" class="travinh__service--thumbnail">
+                                    <img src="https://dulichtravinh.com.vn/images/2017/09/29/tom_kho_vinh_kim.jpg" alt="">
+                                </a>
+                                <div class="travinh__service--content">
+                                    <a href="#" class="travinh__service--link">
+                                        <h4 class="travinh__service--title">Tôm khô Vĩnh Kim</h4>
+                                    </a>
+                                    <p>Giá: 140.000đ</p>
+                                    <button class="travinh__service--addtocart">Thêm vào giỏ hàng</button>
+                                </div>
+                            </li>
+                            <li class="travinh__service--card">
+                                <a href="#" class="travinh__service--thumbnail">
+                                    <img src="https://dulichtravinh.com.vn/images/2017/09/29/tom_kho_vinh_kim.jpg" alt="">
+                                </a>
+                                <div class="travinh__service--content">
+                                    <a href="#" class="travinh__service--link">
+                                        <h4 class="travinh__service--title">Tôm khô Vĩnh Kim</h4>
+                                    </a>
+                                    <p>Giá: 140.000đ</p>
+                                    <button class="travinh__service--addtocart">Thêm vào giỏ hàng</button>
+                                </div>
+                            </li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+            <div class="travinh__posts--tag">
+                <h3 class="travinh__tag--title">Tag phổ biến</h3>
+            </div>
+        </div>
+    </main>
+    <footer>
+
+    </footer>
+    <script>
+        document.addEventListener("DOMContentLoaded", function() {
+            const switchLanguage = document.querySelector("#switch-language");
+            switchLanguage.addEventListener("click", () => {
+                let lang = switchLanguage.getAttribute("alt").toLowerCase();
+
+                if(lang === "vietnam") {
+                    const viJson = <?php include_once "assets/lang/en.json"; ?>;
+                    console.log(viJson.navigation)
+                    switchLanguage.setAttribute("src", "./assets/images/england.png");
+                    switchLanguage.setAttribute("alt", "england");
+                } else {
+                    const viJson = <?php include_once "assets/lang/vi.json"; ?>
+                    console.log(viJson.navigation)
+                    switchLanguage.setAttribute("src", "./assets/images/vietnam.png");
+                    switchLanguage.setAttribute("alt", "vietnam");
+                }
+            })
+        })
+    </script>
+</body>
+</html> 
+ 
